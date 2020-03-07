7a5d768a473def6980fced8614968f5a7765c1c0
AnnieTYF <980315703@qq.com>
2020-03-07 17:47:41 +0800

    单词拆分-回溯法

--- src/main/java/leecode/String/WordBreak.java
+++ src/main/java/leecode/String/WordBreak.java
@@ -1,5 +1,6 @@
 package leecode.String;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class WordBreak {
@@ -30,6 +31,7 @@ public class WordBreak {
      * tips：如果想再加快运行速度，可以添加一行代码
      * Set<String> wordDictSet=new HashSet(wordDict);
      * 取消字典里重复的，这样contains方法效率应该会提高
+     * 13 ms
      * @param s
      * @param wordDict
      * @return
@@ -54,5 +56,30 @@ public class WordBreak {
      * 还有使用树的
      * https://leetcode-cn.com/problems/word-break/solution/dan-ci-chai-fen-by-leetcode/
      */
+    /**
+     * 记忆化回溯
+     * Boolean[] memo  数组会被用来保存子问题的结果，如果子问题为true，父问题才有可能为true
+     * 每当访问到已经访问过的后缀串，直接用 memo 数组中的值返回而不需要继续调用函数
+     * 时间复杂度：O(n^2) 回溯树的大小最多达到 n^2
+     *空间复杂度：O(n) 回溯树的深度可以达到 n 级别。
+     * 8 ms
+     */
+    public boolean wordBreak2(String s, List<String> wordDict) {
+        return word_Break(s, new HashSet(wordDict), 0, new Boolean[s.length()]);
+    }
+    private boolean word_Break(String s , HashSet wordDict,int start,Boolean[] memo){
+        if(start == s.length()){
+           return true;
+        }
+        if(memo[start] != null){
+          return memo[start];
+        }
+        for(int end = start+1;end<=s.length();end++){
+            if(wordDict.contains(s.substring(start,end)) && word_Break(s,wordDict,end,memo)){
+               return memo[start] = true;
+            }
+        }
+        return memo[start] = false;
+    }
 
 }
